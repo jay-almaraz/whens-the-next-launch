7573d962420b1a9c03bdefe83e4c94eead11690f
jay-almaraz <jay.amgrant@gmail.com>
2021-03-26 01:27:58 +1000

    working timezones

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,10 @@ import Layout from "@components/Layout";
 import SpaceXLogo from "@components/SpaceXLogo";
 import { getNextLaunch, Launch } from "@lib/launches";
 import { Box, Label, Select } from "@theme-ui/components";
+import { IANA_TIMEZONES } from "@utils/timezones";
+import { DateTime, LocalZone } from "luxon";
 import Head from "next/head";
+import { useEffect, useState } from "react";
 
 export const SITE_TITLE = "When's the next launch?";
 
@@ -74,7 +77,57 @@ const EmphSpan = ({ children }) => {
   );
 };
 
-const TimezoneSelect = () => {
+interface EmphClockProps {
+  dateTime: DateTime;
+}
+
+const EmphClock = (props: EmphClockProps) => {
+  const { dateTime } = props;
+  const hour = dateTime.toFormat("hh");
+  const minute = dateTime.toFormat("mm");
+  const meridiem = dateTime.toFormat("a").toLocaleLowerCase();
+
+  const [tick, setTick] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTick((prev) => !prev);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [tick]);
+
+  return (
+    <Box
+      as="span"
+      sx={{
+        //
+        fontSize: 36,
+        fontWeight: "bold",
+        margin: 0,
+        padding: 0,
+        marginTop: 1,
+        overflow: "visible",
+      }}
+    >
+      {hour}
+      <Box as="span" sx={{ opacity: tick ? 0 : 1 }}>
+        :
+      </Box>
+      {minute} {meridiem}
+      <br />
+    </Box>
+  );
+};
+
+interface TimezoneSelectProps {
+  localZone: string;
+  timeZone: string;
+  handleTimezoneChange: (tz: string) => void;
+}
+
+const TimezoneSelect = (props: TimezoneSelectProps) => {
+  const { localZone, timeZone, handleTimezoneChange } = props;
+  const hasLocalSelected = localZone === timeZone;
   return (
     <Box as="form">
       <Label
@@ -88,20 +141,36 @@ const TimezoneSelect = () => {
           marginBottom: 2,
         }}
       >
-        your local time
+        {hasLocalSelected ? "your local time" : "in your selected timezone"}
       </Label>
-      <Select defaultValue="(Australia/Brisbane)">
-        <option>(Australia/Brisbane)</option>
+      <Select
+        defaultValue={localZone}
+        onChange={(event) => handleTimezoneChange(event.target.value)}
+      >
+        {IANA_TIMEZONES.map((tz) => (
+          <option key={tz}>{tz}</option>
+        ))}
       </Select>
     </Box>
   );
 };
 
-export default function Home(props: HomeProps) {
-  const { data } = props;
+const LOCAL_FORMAT = Object.assign(DateTime.DATE_FULL, { weekday: "long" });
 
-  const localDate = "Thursday, 1 April 2021";
-  const localTime = "10:00 am";
+export default function Home(props: HomeProps) {
+  const {
+    data: { name, date_utc, details, date_precision },
+  } = props;
+
+  const localZone = new LocalZone();
+  const [timeZone, setTimeZone] = useState<string>(localZone.name);
+  const dateTimeUTC = DateTime.fromISO(date_utc, { zone: "utc" });
+  const dateTimeLocal = dateTimeUTC.setZone(timeZone);
+  const localDate = dateTimeLocal.toLocaleString(LOCAL_FORMAT);
+
+  function handleTimezoneChange(tz: string) {
+    setTimeZone(tz);
+  }
 
   return (
     <Layout>
@@ -114,17 +183,21 @@ export default function Home(props: HomeProps) {
           flexDirection: "column",
         }}
       >
-        <Box sx={{ width: ["100%", 500], overflow: "hidden" }}>
+        <Box as="span" sx={{ width: ["100%", 500], overflow: "hidden" }}>
           <SpaceXLogo />
         </Box>
         <LabelSpan>has scheduled the</LabelSpan>
-        <EmphSpan>{data.name}</EmphSpan>
+        <EmphSpan>{name}</EmphSpan>
         <LabelSpan>mission for</LabelSpan>
         <EmphSpan>{localDate}</EmphSpan>
         <LabelSpan>at</LabelSpan>
-        <EmphSpan>{localTime}</EmphSpan>
-        <TimezoneSelect />
+        <EmphClock dateTime={dateTimeLocal} />
       </Box>
+      <TimezoneSelect
+        localZone={localZone.name}
+        timeZone={timeZone}
+        handleTimezoneChange={handleTimezoneChange}
+      />
     </Layout>
   );
 }
